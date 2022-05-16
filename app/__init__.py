debec742ebfd7a759549f64d86aad647e5b3297d
victoria mwangi <victoriamwangi021@gmail.com>
2022-05-16 13:34:17 +0300

    feat: user registration and login

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap 
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
-
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 
 def create_app(config_name):
@@ -22,4 +24,5 @@ def create_app(config_name):
     
     db.init_app(app)
     bootstrap.init_app(app)
+    login_manager.init_app(app)
     return app
