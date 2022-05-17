e7ef09dc145dd24d96487d66f1994786bc79ef57
halima precious <halimaprecious3@gmail.com>
2022-05-17 11:32:35 +0300

    ft:adds models

--- app/__init__.py
+++ app/__init__.py
@@ -15,6 +15,7 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_options[config_name])
     
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
     
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
